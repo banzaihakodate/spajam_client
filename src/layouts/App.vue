9e17cd8f8b7484128d9a1abda8fdb09332fffa4d
vivid344 <vivid344@icloud.com>
2019-05-11 16:20:03 +0900

    [fix]toolbarの削除

--- src/layouts/App.vue
+++ src/layouts/App.vue
@@ -1,20 +1,5 @@
 <template>
   <v-app>
-    <v-toolbar dark color="primary" class="toolbar">
-      <v-toolbar-title class="white--text">テストだよ</v-toolbar-title>
-      <v-spacer></v-spacer>
-
-      <router-link to="/">
-        <v-btn icon>
-          <v-icon>home</v-icon>
-        </v-btn>
-      </router-link>
-      <router-link to="/About">
-        <v-btn icon>
-          <v-icon>info</v-icon>
-        </v-btn>
-      </router-link>
-    </v-toolbar>
     <v-content class="content">
       <router-view></router-view>
     </v-content>
@@ -22,11 +7,6 @@
 </template>
 
 <style>
-  .toolbar {
-    position: sticky;
-    top: 0;
-    z-index: 1000;
-  }
   .content {
     height: 90vh;
   }
