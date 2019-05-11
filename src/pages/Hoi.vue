d3462e48384159b0d0d19f19bd0955fb1b9c6b3b
youtangai <youtanagai@gmail.com>
2019-05-12 02:51:50 +0900

    [add] ホイの画面に自身のアングルを表示する

--- src/pages/Hoi.vue
+++ src/pages/Hoi.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
-    <h1>ホイ</h1>
+    <div v-if="isDebuf">
+      <div>{{beta}}</div>
+      <div>{{gamma}}</div>
+      <div>{{y}}</div>
+      <div>{{x}}</div>
+    </div>
+    {{angle}}
   </div>
 </template>
 
@@ -12,6 +18,12 @@ export default {
   name: 'home',
   data() {
     return {
+      isDebug:false,
+      beta:0,
+      gamma:0,
+      x:0, 
+      y:0,
+      angle:0
       // このコンポーネントのみで使う値の宣言
     }
   },
@@ -27,6 +39,21 @@ export default {
     ...mapActions([
       // Actionから受け取りたいアクションを選択
     ]),
+    orientationHandler() { //要パラメータ調整
+      this.beta = event.beta //角度
+      this.gamma = event.gamma //角度
+      this.x = Math.sin(event.gamma*Math.PI/180) //ラジアンに変換して正弦波
+      this.y = Math.cos(event.beta*Math.PI/180) //ラジアンに変換して余弦波
+      const _angle = Math.atan2(this.x,this.y)*180/Math.PI //tanの値から，角度算出
+      if (_angle < 0) {
+        this.angle = Math.floor(360 + _angle)
+      } else {
+        this.angle = Math.floor(_angle)
+      }
+    }
+  },
+  created() {
+    window.addEventListener('deviceorientation', this.orientationHandler)
   }
 }
 </script>
